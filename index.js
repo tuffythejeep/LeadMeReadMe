625a1e49fc6377acf0a225148fa4afe442f81853
tuffythejeep <>
2024-07-13 14:35:54 -0500

    updates to index.js

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ inquirer
   .prompt([
     {
       type: "text",
-      message: "Please provide a clear and descriptive title for the project.",
+      message: "Please provide a clear and descriptive title for the project:",
       name: "titleInput",
     },
     {
       type: "text",
-      message: "Please provide a brief overview of the project, explaining its purpose, features, and any other relevant information.",
+      message: "Please provide a brief overview of the project, explaining its purpose, features, and any other relevant information:",
       name: "descriptionInput",
     },
     {
       type: "text",
-      message: "Please provide instructions on how to install and set up the project locally.",
+      message: "Please provide instructions on how to install and set up the project locally:",
       name: "installationInput",
     },
     {
       type: "text",
-      message: "Pleasen list guidelines on how to use the project, including any commands or configurations.",
+      message: "Please list guidelines on how to use the project, including any commands or configurations:",
       name: "usageInput",
     },
     {
       type: "text",
-      message: "Please provide guidelines for contributors, including information on how to submit changes or report issues.",
+      message: "Please provide guidelines for contributors, including information on how to submit changes or report issues:",
       name: "contributionInput",
     },
      {
       type: "text",
-      message: "Please provide acknowledgment of any individuals, libraries, or resources used in the project.",
+      message: "Please provide acknowledgment of any individuals, libraries, or resources used in the project:",
       name: "creditsInput",
     },
     {
       type: "text",
-      message: "Please provide information about the project's license, outlining how others can use and contribute to the project.",
+      message: "Please provide information about the project's license, outlining how others can use and contribute to the project:",
       name: "licenseInput",
     },
     {
       type: "text",
-      message: "Please provide Information on how to run tests and ensure the project's functionality.",
+      message: "Please provide Information on how to run tests and ensure the project's functionality:",
       name: "testsInput",
     },
     {
