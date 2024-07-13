28b8095a87d05a028dc9c79c35cd4f8814cd4d3a
tuffythejeep <>
2024-07-13 14:31:38 -0500

    leadmereadme updates

--- index.js
+++ index.js
@@ -4,64 +4,120 @@ inquirer
   .prompt([
     {
       type: "text",
-      message: "What is your name?",
-      name: "nameInput",
+      message: "Please provide a clear and descriptive title for the project.",
+      name: "titleInput",
     },
     {
       type: "text",
-      message: "Where are you from?",
-      name: "locationInput",
+      message: "Please provide a brief overview of the project, explaining its purpose, features, and any other relevant information.",
+      name: "descriptionInput",
     },
     {
       type: "text",
-      message: "What is your favorite hobby?",
-      name: "hobbyInput",
+      message: "Please provide instructions on how to install and set up the project locally.",
+      name: "installationInput",
     },
     {
       type: "text",
-      message: "What is your favorite food?",
-      name: "foodInput",
+      message: "Pleasen list guidelines on how to use the project, including any commands or configurations.",
+      name: "usageInput",
     },
     {
       type: "text",
-      message: "Enter your GitHub Username:",
+      message: "Please provide guidelines for contributors, including information on how to submit changes or report issues.",
+      name: "contributionInput",
+    },
+     {
+      type: "text",
+      message: "Please provide acknowledgment of any individuals, libraries, or resources used in the project.",
+      name: "creditsInput",
+    },
+    {
+      type: "text",
+      message: "Please provide information about the project's license, outlining how others can use and contribute to the project.",
+      name: "licenseInput",
+    },
+    {
+      type: "text",
+      message: "Please provide Information on how to run tests and ensure the project's functionality.",
+      name: "testsInput",
+    },
+    {
+      type: "text",
+      message: "Enter your GitHub Username, so others can find you there:",
       name: "gihubInput",
     },
     {
       type: "text",
-      message: "Enter your LinkedIn URL:",
+      message: "Enter your LinkedIn URL, for other questions or concers:",
       name: "linkedinInput",
     },
   ])
   .then((response) => {
     const {
-      nameInput,
-      locationInput,
-      hobbyInput,
-      foodInput,
+      titleInput
+      descriptionInput,
+      installationInput,
+      usageInput,
+      contributionInput,
+      creditsInput,
+      licenseInput,
+      testsInput,
       githubInput,
       linkedinInput,
     } = response;
-    const htmlContent = `
-      <html>
-        <head>
-          <title>Portfolio</title>
-        </head>
-        <body>
-          <h1>${nameInput}'s Portfolio</h1>
-          <p>Location: ${locationInput}</p>
-          <p>Hobby: ${hobbyInput}</p>
-          <p>Favorite Food: ${foodInput}</p>
-          <p>GitHub: <a href="https://github.com/${githubInput}">${githubInput}</a></p>
-          <p>LinkedIn: <a href="${linkedinInput}">${linkedinInput}</a></p>
-        </body>
-      </html>
+    const mdContent = `
+# Title
+${titleInput}
+
+## Table of Contents
+
+- [Description](#my-description)
+
+- [Installation Instructions](#my-installation)
+
+- [Usage Information](#my-usage)
+
+- [Contribution Guidelines](#my-contributions)
+
+- [Credits](#my-credits)
+
+- [License](#my-license)
+
+- [Tests](#my-tests)
+
+- [Questions?](#my-questions)
+
+## Description <a name="my-description"></a>
+${descriptionInput}
+
+## Installation <a name="my-installation"></a>
+${installationInput}
+
+## Usage <a name="my-usage"></a>
+${usageInput}
+
+## Contributions <a name="my-contributions"></a>
+${contributionInput}}
+
+## Credits <a name="my-credits"></a>
+${creditsInput}
+
+## License <a name="my-license"></a>
+${licenseInput}
+
+## Tests <a name="my-tests"></a>
+${testsInput}
+
+## Questions <a name="my-questions"></a>
+GitHub: <a href="https://github.com/${githubInput}">${githubInput}</a> in case you want to find me on github!
+LinkedIn: <a href="${linkedinInput}">${linkedinInput}</a> in case you have any other questions or concerns.
     `;
-    fs.writeFile("portfolio.html", htmlContent, (error) => {
+    fs.writeFile("outputReadMe.md", mdContent, (error) => {
       if (error) {
         console.error("Error writing file:", error);
       } else {
-        console.log("Portfolio generated successfully!");
+        console.log("ReadMe generated successfully!");
       }
     });
   });
